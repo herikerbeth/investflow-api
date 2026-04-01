9c31bd61e7b72f4a2fc864e51c0e80fc42623798
herik-erbeth <herik.docarmo@hotmail.com>
2026-04-01 17:43:27 -0300

    test(portfolio): add integration tests using Testcontainers for PortfolioService

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,9 @@ dependencies {
     annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:testcontainers-junit-jupiter")
+    testImplementation("org.testcontainers:testcontainers-mysql")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testRuntimeOnly("com.h2database:h2")
 }
